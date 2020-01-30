53cea769f9667dd74f14362719fbeeef8c19f6ff
Achiaga <alfonso.achiaga@gmail.com>
2020-01-30 19:58:28 +0100

    add snake body-tail

--- snake.js
+++ snake.js
@@ -2,15 +2,17 @@ var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 document.addEventListener('keydown', keyDownEvent);
 
-//draw Snakevar
-squareSnake = 15;
+//draw Snake
+var squareSnake = 15;
 var snakeWidth = squareSnake;
 var snakeX = canvas.width / 2;
 var snakeHeight = squareSnake;
 var snakeY = canvas.height / 2;
+var length = 1;
+var snakeBody = [{ x: snakeX, y: snakeY }];
 
-//draw Foodvar
-foodX = 10;
+//draw Food
+var foodX = 10;
 var foodY = 10;
 
 //snake
@@ -23,19 +25,26 @@ var Y = false;
 score = 0;
 
 function drawSnake() {
-	ctx.beginPath();
-	ctx.rect(snakeX, snakeY, snakeWidth, snakeHeight);
-	ctx.fillStyle = 'red';
-	ctx.fill();
-	ctx.closePath();
+	for (var i = 0; i < length; i++) {
+		ctx.beginPath();
+		ctx.rect(snakeBody[i].x, snakeBody[i].y, snakeWidth, snakeHeight);
+		ctx.fillStyle = 'red';
+		ctx.fill();
+		ctx.closePath();
+	}
+	// ctx.beginPath();
+	// ctx.rect(snakeBody[0].x, snakeBody[0].y, snakeWidth, snakeHeight);
+	// ctx.fillStyle = 'red';
+	// ctx.fill();
+	// ctx.closePath();
 }
 function foodCoordinates() {
 	if (foodX % 15 !== 0 || foodY % 15 !== 0) {
 		while (foodX % 15 !== 0) {
-			foodX = Math.floor(Math.random() * 100);
+			foodX = Math.floor(Math.random() * 480);
 		}
 		while (foodY % 15 !== 0) {
-			foodY = Math.floor(Math.random() * 100);
+			foodY = Math.floor(Math.random() * 330);
 		}
 	}
 }
@@ -52,13 +61,13 @@ function drawFood() {
 	}
 }
 function checkCollision() {
-	if (snakeX === foodX && snakeY === foodY) {
+	if (snakeBody[0].x === foodX && snakeBody[0].y === foodY) {
 		eatFood();
 	} else if (
-		snakeX < 0 ||
-		snakeX > canvas.width - squareSnake ||
-		snakeY < 0 ||
-		snakeY > canvas.height - squareSnake
+		snakeBody[0].x < 0 ||
+		snakeBody[0].x > canvas.width - squareSnake ||
+		snakeBody[0].y < 0 ||
+		snakeBody[0].y > canvas.height - squareSnake
 	) {
 		return true;
 	} else {
@@ -67,6 +76,8 @@ function checkCollision() {
 }
 function eatFood() {
 	score++;
+	length++;
+	snakeBody.push({ x: foodX, y: foodY });
 	foodX = 10;
 	foodY = 10;
 	drawFood();
@@ -143,8 +154,13 @@ function draw() {
 	drawGrid();
 	drawFood();
 	drawScore();
-	snakeX += movX;
-	snakeY += movY;
+	for (var i = length - 1; i > 0; i--) {
+		snakeBody[i].x = snakeBody[i - 1].x;
+		snakeBody[i].y = snakeBody[i - 1].y;
+	}
+	snakeBody[0].x += movX;
+	snakeBody[0].y += movY;
+	console.log(snakeBody);
 	if (checkCollision()) {
 		clearInterval(game);
 		return;
